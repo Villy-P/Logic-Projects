ddeacc026d05487046137a2add457e973252df0d
Villy-P <valeriuspetrini@gmail.com>
2024-09-10 14:36:14 -0500

    style: format main.ts

--- chess/src/main.ts
+++ chess/src/main.ts
@@ -1,22 +1,28 @@
 class World {
-    public static readonly canvas : HTMLCanvasElement        = <HTMLCanvasElement> document.getElementById("mainCanvas");
-    public static readonly context: CanvasRenderingContext2D = World.canvas.getContext("2d")!;
-    public static readonly boundingClient: DOMRect = World.canvas.getBoundingClientRect();
-    public static readonly offsetX: number = World.boundingClient.left;
-    public static readonly offsetY: number = World.boundingClient.top;
-    public static readonly colorPickers: ColorInput = new ColorInput();
-    public static readonly history: HistoryCanvas = new HistoryCanvas();
-    public static drawCounter: number = 0;
-    public static dragging: boolean = false;
-    public static isWhiteTurn: boolean = true;
+	public static readonly canvas: HTMLCanvasElement = <HTMLCanvasElement>(
+		document.getElementById("mainCanvas")
+	);
+	public static readonly context: CanvasRenderingContext2D =
+		World.canvas.getContext("2d")!;
+	public static readonly boundingClient: DOMRect =
+		World.canvas.getBoundingClientRect();
+	public static readonly offsetX: number = World.boundingClient.left;
+	public static readonly offsetY: number = World.boundingClient.top;
+	public static readonly colorPickers: ColorInput = new ColorInput();
+	public static readonly history: HistoryCanvas = new HistoryCanvas();
+	public static drawCounter: number = 0;
+	public static dragging: boolean = false;
+	public static isWhiteTurn: boolean = true;
 
-    public readonly board: Board = new Board();
+	public readonly board: Board = new Board();
 
-    public constructor() {
-        World.canvas.width  = (DevSettings.boxDimensions * 8) + DevSettings.numberLetterDimensions;
-        World.canvas.height = (DevSettings.boxDimensions * 8) + DevSettings.numberLetterDimensions;
-        World.canvas.onmousedown = KeyEvents.mouseDown;
-        World.canvas.onmouseup = KeyEvents.mouseUp;
-        World.canvas.onmousemove = KeyEvents.mouseMove;
-    }
+	public constructor() {
+		World.canvas.width =
+			DevSettings.boxDimensions * 8 + DevSettings.numberLetterDimensions;
+		World.canvas.height =
+			DevSettings.boxDimensions * 8 + DevSettings.numberLetterDimensions;
+		World.canvas.onmousedown = KeyEvents.mouseDown;
+		World.canvas.onmouseup = KeyEvents.mouseUp;
+		World.canvas.onmousemove = KeyEvents.mouseMove;
+	}
 }
