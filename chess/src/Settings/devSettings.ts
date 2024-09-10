fda8293c10c5f3629f07d13fa6548c41fdf0311f
Villy-P <valeriuspetrini@gmail.com>
2024-09-10 14:35:03 -0500

    style: format devsettings.ts

--- chess/src/Settings/devSettings.ts
+++ chess/src/Settings/devSettings.ts
@@ -1,8 +1,8 @@
 class DevSettings {
-    public static readonly boxDimensions: number = 50;
+	public static readonly boxDimensions: number = 50;
 
-    public static readonly numberLetterDimensions: number = 50;
-    public static readonly numberLetterFont: number = 50;
-    public static readonly lineWidth: number = 2;
-    public static readonly pieceDimensions: number = 40;
+	public static readonly numberLetterDimensions: number = 50;
+	public static readonly numberLetterFont: number = 50;
+	public static readonly lineWidth: number = 2;
+	public static readonly pieceDimensions: number = 40;
 }
