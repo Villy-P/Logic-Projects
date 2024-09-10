15857092622ca85d21f07632e6e0b8a626de36fa
Villy-P <valeriuspetrini@gmail.com>
2024-09-10 14:35:16 -0500

    style: format usersettings.ts

--- chess/src/Settings/userSettings.ts
+++ chess/src/Settings/userSettings.ts
@@ -1,6 +1,6 @@
 class UserSettings {
-    public static whiteSquareColor: string = "white";
-    public static blackSquareColor: string = "#006400";
+	public static whiteSquareColor: string = "white";
+	public static blackSquareColor: string = "#006400";
 
-    public static fileBeginning: string = "images/Basic/"
+	public static fileBeginning: string = "images/Basic/";
 }
