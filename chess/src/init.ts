cc5c0c4a6f5317f985c81128403c3de199afcddb
Villy-P <valeriuspetrini@gmail.com>
2024-09-10 14:36:04 -0500

    style: format init.ts

--- chess/src/init.ts
+++ chess/src/init.ts
@@ -1,17 +1,22 @@
 const world: World = new World();
 
 async function main(): Promise<void> {
-    await Functions.loadImages();
-    world.board.drawBoard();
+	await Functions.loadImages();
+	world.board.drawBoard();
 }
 
 main();
 
 function run() {
-    world.board.drawBoard();
-    world.board.drawPieces();
-    World.history.context.clearRect(0, 0, World.history.canvas.width, World.history.canvas.height);
-    World.history.print();
+	world.board.drawBoard();
+	world.board.drawPieces();
+	World.history.context.clearRect(
+		0,
+		0,
+		World.history.canvas.width,
+		World.history.canvas.height
+	);
+	World.history.print();
 }
 
 setInterval(run, 33);
