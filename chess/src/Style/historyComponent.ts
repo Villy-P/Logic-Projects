614016c3747abeec79cd791acda4f96bbc440d4b
Villy-P <valeriuspetrini@gmail.com>
2024-09-10 14:35:51 -0500

    style: format historycomponent

--- chess/src/Style/historyComponent.ts
+++ chess/src/Style/historyComponent.ts
@@ -1,9 +1,9 @@
 class HistoryComponent {
-    public move1: string;
-    public move2: string;
+	public move1: string;
+	public move2: string;
 
-    public constructor(move1: string, move2: string) {
-        this.move1 = move1;
-        this.move2 = move2;
-    }
+	public constructor(move1: string, move2: string) {
+		this.move1 = move1;
+		this.move2 = move2;
+	}
 }
